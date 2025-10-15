837e0aa5669a81787e25aa9787ef93008d656304
MARYAMTEE <ayorindemaryam078@gmail.com>
2025-10-15 18:55:52 +0100

    update changes

--- js/script.js
+++ js/script.js
@@ -2,14 +2,19 @@ const openMenu = document.querySelector(".open__menu");
 const closeMenu = document.querySelector(".close__menu");
 const navMenu = document.querySelector(".nav__menu");
 const navList = document.querySelectorAll(".nav__item");
-const navLink = document.querySelectorAll(".nav__link")
+const navLink = document.querySelectorAll(".nav__link");
+const logoText = document.querySelector(".logo");
 
 openMenu.addEventListener("click", () => {
     navMenu.classList.toggle("toggle");
+
+    logoText.classList.add("hidden");
 });
 
 closeMenu.addEventListener("click", () => {
     navMenu.classList.remove("toggle");
+
+    logoText.classList.remove("hidden")
 });
 
 navList.forEach((list, index) => {
